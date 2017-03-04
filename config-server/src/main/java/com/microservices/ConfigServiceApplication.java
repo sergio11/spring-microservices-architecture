fbd67a9b808b12e17b8d05867eaac1c0a112ace8
Sergio Sánchez <sss4esob@gmail.com>
2017-03-04 12:26:35 +0100

    feat(config): add Spring Cloud Config microservice

---
+++ config-server/src/main/java/com/microservices/ConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+}
