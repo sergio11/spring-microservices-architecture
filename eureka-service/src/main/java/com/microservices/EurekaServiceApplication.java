35583b07e6b2c69200f8538ea35e749f5ca1e99e
Sergio Sánchez <sss4esob@gmail.com>
2017-03-04 21:15:51 +0100

    feat(eureka): add Spring Boot Admin for actuators endpoints.

--- eureka-service/src/main/java/com/microservices/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/microservices/EurekaServiceApplication.java
@@ -1,11 +1,13 @@
 package com.microservices;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAdminServer
 public class EurekaServiceApplication {
 
     public static void main(String[] args) {
