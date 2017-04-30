20a68b010031aeed2bb478f425e2d75ea433f2f4
Sergio Sánchez <sss4esob@gmail.com>
2017-04-30 12:59:42 +0200

    feat(users_manadgement): implement UsersManadgementMessageType with Visitor Pattern for proccess messages

--- spring-boot-starters/spring-boot-starter-commons/src/main/java/sanchez/sergio/messages/admin/UsersManadgementMessageType.java
+++ spring-boot-starters/spring-boot-starter-commons/src/main/java/sanchez/sergio/messages/admin/UsersManadgementMessageType.java
@@ -1,9 +1,28 @@
 package sanchez.sergio.messages.admin;
 
 /**
- *
+ * Enum with Visitor pattern
  * @author sergio
  */
 public enum UsersManadgementMessageType {
-    USER_CONNECTED
+    
+    USER_CONNECTED {
+        @Override
+        public void accept(UsersManadgementVisitor visitor, Long idUser) {
+            visitor.visitUserConnected(idUser);
+        }
+    },
+    USER_DISCONNECTED {
+        @Override
+        public void accept(UsersManadgementVisitor visitor, Long idUser) {
+            visitor.visitUserDisconnected(idUser);
+        }
+    };
+    
+    public abstract void accept( UsersManadgementVisitor visitor, Long idUser );
+
+    public interface UsersManadgementVisitor {
+        void visitUserConnected(Long idUser);
+        void visitUserDisconnected(Long idUser);
+    }
 }
