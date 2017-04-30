a3adb0506291b9e51fb1a0f2d337c16c2d2e8177
Sergio Sánchez <sss4esob@gmail.com>
2017-04-30 13:01:17 +0200

    feat(users_manadgement): implement UsersManadgementVisitor on AdminHandler

--- offline-notification-storage/src/main/java/sanchez/sergio/handlers/AdminHandler.java
+++ offline-notification-storage/src/main/java/sanchez/sergio/handlers/AdminHandler.java
@@ -6,17 +6,29 @@ import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sanchez.sergio.messages.admin.UsersManadgementMessage;
+import sanchez.sergio.messages.admin.UsersManadgementMessageType.UsersManadgementVisitor;
 
 /**
  * @author sergio
  */
 @Component("adminHandler")
-public class AdminHandler {
+public class AdminHandler implements UsersManadgementVisitor {
     
     private static Logger logger = LoggerFactory.getLogger(AdminHandler.class);
     
     @RabbitListener(queues="#{rabbitCustomProperties.adminExchange.queues['users'].name}")
     public void handleMessage(@Payload UsersManadgementMessage message) {
         logger.info("Message -> " + message);
+        message.getType().accept(this, message.getId());
+    }
+
+    @Override
+    public void visitUserConnected(Long idUser) {
+        logger.info("Declare new queue for user: " + idUser );
+    }
+
+    @Override
+    public void visitUserDisconnected(Long idUser) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
