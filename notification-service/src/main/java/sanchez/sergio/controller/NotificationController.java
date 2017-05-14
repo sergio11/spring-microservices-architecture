e2faa821d39e8fded3ba82c885c254aedb71d78a
Sergio Sánchez <sss4esob@gmail.com>
2017-05-14 15:05:09 +0200

    feat(api): Add get self notifications

--- notification-service/src/main/java/sanchez/sergio/controller/NotificationController.java
+++ notification-service/src/main/java/sanchez/sergio/controller/NotificationController.java
@@ -7,11 +7,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sanchez.sergio.persistence.repositories.NotificationRepository;
+import sanchez.sergio.security.CommonUserDetailsAware;
 
 /**
  * @author sergio
@@ -28,6 +30,14 @@ public class NotificationController {
     @ApiOperation(value = "getNotifications", nickname = "getNotifications", notes="Get Notifications", response = ResponseEntity.class)
     public ResponseEntity getNotifications(@ApiParam(value = "userId", required = true) @PathVariable Long userId) {
         return Optional.ofNullable(notificationRepository.findByUserId(userId))
+                .map(e -> new ResponseEntity<>(e, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    
+    @GetMapping(path = "/notifications/self")
+    @ApiOperation(value = "getSelfNotifications", nickname = "getSelfNotifications", notes="Get Self Notifications", response = ResponseEntity.class)
+    public ResponseEntity getNotifications(@AuthenticationPrincipal CommonUserDetailsAware<Long> principal) {
+        return Optional.ofNullable(notificationRepository.findByUserId(principal.getUserId()))
                 .map(e -> new ResponseEntity<>(e, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
