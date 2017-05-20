2d6cfe15c2ed39d3436866a1cc0d5d60f38e7131
Sergio Sánchez <sss4esob@gmail.com>
2017-05-20 23:00:12 +0200

    refactor(all): change all bootstrap config. Using Discover first approach.

--- config-service/src/main/java/sanchez/sergio/ConfigServiceApplication.java
+++ config-service/src/main/java/sanchez/sergio/ConfigServiceApplication.java
@@ -2,9 +2,11 @@ package sanchez.sergio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServiceApplication {
 
